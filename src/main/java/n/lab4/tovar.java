e09bf62949a8ad5884413ec9a3502c5b04295916
Faiwolls <107765175+Faiwolls@users.noreply.github.com>
2024-01-23 00:59:45 +0700

    Update tovar.java

--- src/main/java/n/lab4/tovar.java
+++ src/main/java/n/lab4/tovar.java
@@ -10,9 +10,9 @@ package n.lab4;
  */
 public class tovar {
     
-	public int tovnum;
-	public String tovname;
-	public int tkol;
+	public int tovnum; //номер товара
+	public String tovname; //наименование товара
+	public int tkol;// количество товара на складе
         public tovar(){
           tovnum = 0;
           tovname = "";
